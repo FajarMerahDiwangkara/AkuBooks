8e5e5b1cc54c5f74d80be3b8c3c8e7c9c2c949e4
Fajar Merah Diwangkara <fajarmerah2020@mail.ugm.ac.id>
2023-09-17 16:11:23 +0700

    added backend server

---
+++ backend/app/Controllers/DashboardHomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class DashboardHomeController extends BaseController
+{
+    public function index(): string
+    {
+        return "DashboardHomeIndex";
+    }
+}
+?>
---
+++ backend/app/Controllers/DashboardKaryawanController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class DashboardKaryawanController extends BaseController
+{
+    public function index()
+    {
+        return "DashboardKaryawanIndex";
+    }
+}
+?>
